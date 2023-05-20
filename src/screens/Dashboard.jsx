4eb2e3e90940fb712e718703a8551790b18f6fa0
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2023-05-20 14:13:22 +0500

    Update Dashboard.jsx

--- src/screens/Dashboard.jsx
+++ src/screens/Dashboard.jsx
@@ -1,8 +1,9 @@
+import { Link } from "react-router-dom";
 import { useAtom } from "jotai";
 import { userAtom } from "../global/userAtom";
 
 export default function Dashboard() {
-  const [user] = useAtom(userAtom);
+  const [user, setUser] = useAtom(userAtom);
   return (
     <div
       style={{
@@ -14,9 +15,36 @@ export default function Dashboard() {
         width: "100%",
       }}
     >
+      <img
+        src={user?.photoURL}
+        alt={user?.displayName}
+        style={{
+          height: "100px",
+          width: "100px",
+          borderRadius: "50%",
+          marginBottom: "1em",
+        }}
+      />
       <div style={{ fontSize: "2rem", fontWeight: "bold" }}>
         Welcome {user?.displayName}
       </div>
+      <Link
+        to="/"
+        onClick={() => {
+          setUser(null);
+        }}
+        style={{
+          marginTop: "1em",
+          padding: ".5em 1em",
+          color: "white",
+          borderRadius: "5px",
+          cursor: "pointer",
+          backgroundColor: "#242424",
+          textDecoration: "none",
+        }}
+      >
+        Logout
+      </Link>
     </div>
   );
 }
